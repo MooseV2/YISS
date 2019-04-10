42ffe7190c61a4f7e80c7062e7ef58d0c597ec7c
Chirag Karia <trichiragkaria@gmail.com>
2019-04-10 00:19:54 -0400

    Add comments

--- app.py
+++ app.py
@@ -9,14 +9,18 @@ import requests
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from flask_uuid import FlaskUUID
 
+# Change following to '0.0.0.0' and '8080' if running locally
 HOST_IP = 'yourinference.ml'
 HOST_PORT = '80'
 
+# initiate flask app
 app = Flask(__name__)
 FlaskUUID(app)
 
+# connect to docker client
 docker_client = docker.from_env()
 
+# initialize ip lookup
 ip_dict = None
 if os.path.isfile('proxy.p'):
     with open('proxy.p', 'rb') as proxy_file:
@@ -31,9 +35,11 @@ def index():
     Renders the default page for uploading images
     :return: index.html template
     """
+    # open shelve db
     with shelve.open("persistent_data") as db:
         model_info = {k:v["name"] for (k,v) in db.items()}
 
+    # render gallery with existing models
     return render_template('index.html', gallery=model_info)
 
 
@@ -49,22 +55,28 @@ def model(uuid):
     :param uuid: The UUID of the results page
     :return: model.html template, or 404.html on error
     """
+
+    # get provided UUID
     uuid = str(uuid)
+
+    # prepare URL
     url = "{}:{}/models".format(HOST_IP, HOST_PORT)
-    # try:
-    post = request.method == 'POST'
 
-    if post:
-        img_file = request.files['file']
-    else:
-        img_file = None
-    
-    name, desc, output = load_result(uuid, post, img_file=img_file)
-    # except: # UUID wasn't found, throw 404
-    #     return '404 :('
-    #     # TODO: 404.html
-    #     # return render_template('404.html')
+    # if POST, send file to relevant container
+    try:
+        post = request.method == 'POST'
 
+        if post:
+            img_file = request.files['file']
+        else:
+            img_file = None
+
+        # Get result of inference from relevant model
+        name, desc, output = load_result(uuid, post, img_file=img_file)
+    except Exception as e:  # UUID wasn't found, throw 404
+        return '404 :('
+
+    # if not post, then Model page is being requested.
     if not post:
         return render_template('model.html', 
                                 url=url,
@@ -72,21 +84,24 @@ def model(uuid):
                                 description=desc, 
                                 output=output, 
                                 uuid=uuid)
+
+    # if POST, then only return class label
     else:
         return output
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    """
-    Uploads the image from the form data and starts the analysis in a thread
-    :return: string 'SUCCESS' on success, 'FAIL' on failure
-    """
-    # Ensure the user has added a file and email
+    """ Uploads the model and metadata from the form data, creates the new container.
 
+    :return: web view
+    """
+
+    # show model upload page if GET request
     if request.method == 'GET':
         return render_template('upload.html')
 
+    # grab data from request
     try:
         onnx = request.files['onnx']
         thumb = request.files['thumbnail']
@@ -98,21 +113,22 @@ def upload():
         import traceback
         return traceback.print_exc()
 
-    # Otherwise, save the file
+    # generate random uuid
     uuid = str(uuid4())[:8]
-    
+
+    # save thumbnail and demo image under images folder
     thumb_path = os.path.join('images', f'thumb-{uuid}.jpg')
     thumb.save(thumb_path)
-    
     demo_path = os.path.join('images', f'demo-{uuid}.jpg')
     demo.save(demo_path)
 
-    # Create directory for model
+    # Create directory for model and save it
     model_dir = os.path.join('models', uuid)
     os.mkdir(model_dir)
     model_path = os.path.join(model_dir, 'model.onnx')
     onnx.save(model_path)
 
+    # store paths and metadata
     with shelve.open("persistent_data") as db:
         db[uuid] = {
             "onnx": model_path,
@@ -122,28 +138,30 @@ def upload():
             "demo": demo_path
         }
 
+    # save label pickle file under model dir
     label_path = os.path.join(model_dir, 'label.p')
     label_list = labels.split(', ')
-
     with open(label_path, 'wb') as label_file:
         pickle.dump(label_list, label_file)
 
+    # build and start container
     docker_client.images.build(path=".", tag=uuid, buildargs={'model': uuid})
     docker_client.containers.run(uuid, detach=True, name=uuid)
 
+    # check ip of running container and save it in lookup
     ip = subprocess.check_output(
         ["docker", "inspect", "-f", "'{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'", str(uuid)],
         shell=False
     )
-
     ip = ip.decode('utf-8').rstrip()
     print(ip)
     ip_dict[uuid] = ip
 
+    # save proxy file
     with open("proxy.p", 'wb') as _proxy_file:
         pickle.dump(ip_dict, _proxy_file)
 
-    # TODO: Instantiate model container
+    # return index if successful
     return redirect(url_for('index'))
 
 
@@ -152,29 +170,33 @@ def load_result(uuid, post, img_file=None):
         Returns: JSON on success, None on failure
     """
 
+    # extract string if bytes
     if type(uuid) is bytes:
         uuid = uuid.decode('utf-8')
 
+    # get metadata from db
     with shelve.open("persistent_data") as db:
         model = db[uuid]
         name = model["name"]
         desc = model["description"]
         demo = model["demo"]
 
+    # if viewing model page, get demo image
     if not post:
         img_file = open(demo, "rb")
 
+    # get ip of relevant container
     model_ip = ip_dict[uuid]
     model_endpoint = "http://{}:5001/get_prediction".format(model_ip[1:-1])
     print(model_endpoint)
 
+    # get classification
     r = requests.post(model_endpoint, files={'file': img_file}).text
     print(r)
 
+    # return result
     return (name, desc, r)
-
-    # TODO
 
 
 if __name__ == '__main__':
-    app.run(host=HOST_IP, port=HOST_PORT)  # Start the server
+    app.run(host=HOST_IP, port=int(HOST_PORT))  # Start the server
--- include/modelserver.py
+++ include/modelserver.py
@@ -5,40 +5,51 @@ import onnxruntime as rt
 from PIL import Image
 from flask import Flask, request
 
+# initiate flask
 app = Flask(__name__)
 
+# load onnx model
 sess = rt.InferenceSession("/home/model/model.onnx")
 
+# load model labels
 with open("/home/model/labels.p", "rb") as f:
     labels = pickle.load(f)
 
+# save input and output shapes
 input_shape = sess.get_inputs()[0].shape[1:]
 output_shape = sess.get_outputs()[0].shape[-1]
 print(input_shape, output_shape)
 
 
 def preprocess_img(image):
+    """Processes a given image
+
+    :param image: (PIL Image) The queried image
+    :return: (Numpy Array) preprocessed image
+    """
+    # Check if model requires grayscale or RGB
     if input_shape[0] == 1:
         img = image.convert("L")
     elif input_shape[0] == 3:
         img = image.convert("RGB")
-
-        # b, g, r = img.split()
-        # img = Image.merge("RGB", (r, g, b))
     else:
         raise AttributeError("Invalid number of image channels for model input layer")
 
+    # resize image to relevant shape
     img = img.resize((input_shape[1], input_shape[2]))
     img_array = np.array(img).astype('float32')
 
     print(img_array.shape)
     print(img_array.min(), img_array.max())
 
+    # reshape img to order expected by model
     img_array = img_array.reshape([input_shape[0], input_shape[1], input_shape[2]])
 
+    # pixel scaling for grayscale input
     if input_shape[0] == 1:
         img_array /= 255 if input_shape[1] == 28 else 1
 
+    # pixel scaling for RGB input (WIP)
     elif input_shape[0] == 3:
         print(img_array.ndim)
         img_array = img_array[::-1, ...]
@@ -48,6 +59,7 @@ def preprocess_img(image):
         img_array[1, :, :] -= mean[1]
         img_array[2, :, :] -= mean[2]
 
+    # set batch size as 1
     img_array = img_array.reshape([1, input_shape[0], input_shape[1], input_shape[2]])
     print(img_array.shape)
     print(img_array.min(), img_array.max())
@@ -57,18 +69,28 @@ def preprocess_img(image):
 
 @app.route('/get_prediction', methods=['POST'])
 def get_prediction():
+    """ Endpoint exposed by flask for performing inference
+
+    :return: (String) Predicted Label
+    """
+
+    # retrieve image from request and preprocess it
     req = request
     img = Image.open(req.files['file'])
     img_array = preprocess_img(img)
 
+    # get input layers of Model
     input_name = sess.get_inputs()[0].name
     label_name = sess.get_outputs()[0].name
 
     print(img_array.shape)
     print(input_shape)
 
+    # Run inference
     pred = sess.run([label_name], {input_name: img_array})[0]
     print(pred.shape)
+
+    # Get class and relevant class label
     index = pred.argmax(axis=1)[0]
     try:
         return labels[index]
